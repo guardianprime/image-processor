9ede7d96b3e1d7206c0de1d68264885e1afb2460
guardianprime <okongordian@gmail.com>
2025-07-25 09:05:20 +0100

    chore: content for the html pages

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
