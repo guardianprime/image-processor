9959f2e2d31d40e1d1f18f4b4bc6d6403fa5c18b
guardianprime <okongordian@gmail.com>
2025-07-25 09:13:25 +0100

    chore: mongodb connection setup

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+const { connectToMongoDB } = require("./db");
+const bodyparser = require("body-parser");
 
 const app = express();
+connectToMongoDB();
 
 const PORT = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
