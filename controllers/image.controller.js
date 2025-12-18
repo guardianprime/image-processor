cb0f6142de3216a3b4405d09aa8cbcb08f9324d2
guardianprime <okongordian@gmail.com>
2025-12-18 23:05:44 +0100

    chore: first multer npm package trial with image uploads

---
+++ controllers/image.controller.js
@@ -0,0 +1,10 @@
+export const uploadImageController = (req, res) => {
+  console.log("This is the Request file", req.file);
+  res.status(201).json({
+    success: true,
+    data: req.file,
+  });
+};
+export const transformImageController = (req, res) => {};
+export const getImageController = (req, res) => {};
+export const getImagesController = (req, res) => {};
--- routes/images.routes.js
+++ routes/images.routes.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
+
+const upload = multer({ dest: "data/" });
+
+import {
+  uploadImageController,
+  transformImageController,
+  getImageController,
+  getImagesController,
+} from "../controllers/image.controller.js";
 
 const imageRouter = Router();
 
-imageRouter.get("/", (req, res) => {
-  res.send("image endpoint working");
-});
+imageRouter.post("/images", upload.single("test"), uploadImageController);
+
+imageRouter.post("/images/:id/transform", transformImageController);
+
+imageRouter.get("/images", getImagesController);
+
+imageRouter.get("/images/:id", getImageController);
 
 export default imageRouter;
